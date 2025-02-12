3a3b2971b6503743a184cac9e29457827da4468d
haerimee <gofla728@gmail.com>
2025-02-12 23:34:41 +0900

    [feat] 웹툰 등록, 수정, 삭제 기능 추가

---
+++ src/main/java/com/toongather/toongather/domain/webtoon/repository/GenreKeywordRepository.java
@@ -0,0 +1,14 @@
+package com.toongather.toongather.domain.webtoon.repository;
+
+import com.toongather.toongather.domain.webtoon.domain.GenreKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+public interface GenreKeywordRepository extends JpaRepository<GenreKeyword, Long> {
+
+    List<GenreKeyword> findAllByGenreKeywordIdIn(Collection<Long> genreKeywordIds);
+}
