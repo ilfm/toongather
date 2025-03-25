7ddbb4e62de8859c3a1af3b857110221fc94f38f
ilfm <dnehd1828@gmail.com>
2025-03-25 16:33:31 +0900

    [feature,test] 리뷰 기록 조회, 친구에게 평가 공유 조회 기능 추가

--- src/main/java/com/toongather/toongather/domain/review/service/ReviewService.java
+++ src/main/java/com/toongather/toongather/domain/review/service/ReviewService.java
@@ -6,19 +6,20 @@ import com.toongather.toongather.domain.keyword.service.KeywordService;
 import com.toongather.toongather.domain.member.domain.Member;
 import com.toongather.toongather.domain.member.service.MemberService;
 import com.toongather.toongather.domain.review.domain.Review;
-import com.toongather.toongather.domain.review.domain.ReviewRecord;
 import com.toongather.toongather.domain.review.domain.ReviewSortType;
 import com.toongather.toongather.domain.review.dto.CreateReviewRequest;
 import com.toongather.toongather.domain.review.dto.ReviewDto;
-import com.toongather.toongather.domain.review.dto.ReviewRecordDto;
+import com.toongather.toongather.domain.review.dto.ReviewShareRequest;
+import com.toongather.toongather.domain.review.dto.ReviewShareResponse;
 import com.toongather.toongather.domain.review.dto.UpdateReviewRequest;
 import com.toongather.toongather.domain.review.repository.ReviewRepository;
 import com.toongather.toongather.domain.webtoon.domain.Webtoon;
 import com.toongather.toongather.domain.webtoon.repository.WebtoonRepository;
 import com.toongather.toongather.global.common.util.file.FileStore;
 
-import java.util.ArrayList;
+
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,15 +61,6 @@ public class ReviewService {
   private final FileStore fileStore;
   private final int REVIEW_PAGE_SIZE = 10;
 
-  /**
-   * 정렬 기준에 따라 리뷰 목록을 조회하는 메서드
-   *
-   * @param reviewSortType 정렬 기준 (STAR_ASC, STAR_DESC, CREATE_DATE_DESC 등)
-   * @param pageable       페이지네이션 정보 (페이지 번호, 크기 등)
-   * @return 정렬된 리뷰 목록 (페이지네이션 적용)
-   * @implNote - 정렬 기준이 null이면 기본값으로 최신 등록일순(CREATE_DATE_DESC)을 적용. - Pageable의 페이지 번호를 유지하면서, 지정된
-   * REVIEW_PAGE_SIZE를 적용하여 페이지네이션 수행. - Review 엔티티를 ReviewDto 변환하여 반환.
-   */
   public Page<ReviewDto> findAllWithSortType(ReviewSortType reviewSortType, Pageable pageable) {
 
     Sort sort;
@@ -103,12 +95,12 @@ public class ReviewService {
             .build());
   }
 
-  /**
-   * 리뷰 엔티티 저장 (이미 존재하는 리뷰가 아닐 경우 저장)
-   *
-   * @param review 저장할 리뷰 엔티티
-   * @return 저장된 리뷰의 ID
-   */
+  public List<ReviewShareResponse> shareReviewWithFriend(ReviewShareRequest request) {
+    List<Review> reviews = reviewRepository.findAllById(request.getReviewIds());
+    return reviews.stream().map(review -> ReviewShareResponse.from(review))
+        .collect(Collectors.toList());
+  }
+
   @Transactional
   public Long saveReview(Review review) {
     if (review.getReviewId() == null) {
@@ -117,12 +109,6 @@ public class ReviewService {
     return review.getReviewId();
   }
 
-  /**
-   * 리뷰 등록
-   *
-   * @param request 리뷰 등록 요청 객체 (멤버 ID, 웹툰 ID, 리뷰 내용, 별점, 키워드 목록 등 포함)
-   * @return 생성된 리뷰의 ID
-   */
   @Transactional
   public Long createReview(CreateReviewRequest request) {
     // 멤버, 웹툰 엔티티조회
@@ -154,20 +140,11 @@ public class ReviewService {
     return review;
   }
 
-  /**
-   * 특정 리뷰 조회
-   */
   public Review findById(Long reviewId) {
     return reviewRepository.findById(reviewId)
         .orElseThrow(() -> new NoSuchElementException("리뷰를 찾을 수 없습니다: " + reviewId));
   }
 
-  /**
-   * 리뷰 수정
-   *
-   * @param request 리뷰 수정 요청 객체 (수정할 리뷰 ID, 추천 코멘트, 별점, 키워드 목록 포함)
-   * @throws NoSuchElementException 수정할 리뷰가 존재하지 않을 경우 예외 발생
-   */
   @Transactional
   public void updateReview(UpdateReviewRequest request) {
     Review review = reviewRepository.findById(request.getReviewId())
@@ -183,23 +160,6 @@ public class ReviewService {
         reviewKeywordService.createReviewKeyword(review, keyword);
       }
     }
-  }
-
-  /*
-   * 나의 기록 리뷰 리스트 조회
-   * */
-  public List<ReviewRecordDto> findReviewRecordList(Long reviewId) {
-    List<ReviewRecord> reviewRecords = reviewRecordRepository.selectReviewRecordList(reviewId);
-    List<ReviewRecordDto> reviewRecordDtos = new ArrayList<>();
-    reviewRecords.forEach(reviewRecord -> {
-      reviewRecordDtos.add(
-          ReviewRecordDto.builder().reviewRecordId(reviewRecord.getReviewRecordId())
-              .reviewId(reviewRecord.getReview().getReviewId()).record(reviewRecord.getRecord())
-              .amdDt(reviewRecord.getAmdDt())
-              .build());
-    });
-
-    return reviewRecordDtos;
   }
 
   @Transactional
